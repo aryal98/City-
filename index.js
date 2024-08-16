61bc369b3ed8aad4563b3ad5cadabce3b59ce608
aryalakash0505 <aryalakash0505@gmail.com>
2024-08-16 16:17:28 +0545

    Hello

---
+++ index.js
@@ -0,0 +1,24 @@
+// const myFunc=()=>{
+//     console.log("hi")
+//     console.log(document.querySelector(".akash").classList)
+//     document.querySelector(".akash").classList.toggle("my-class")  
+
+// }
+
+
+// function myFunc2(()=>myFunc3){
+
+// }
+
+
+// const myFunc3=()=>{
+//     return "test"
+// }
+var firstName="Akash"
+    let lastName="Aryal"
+    {
+        console.log("firstName")
+        console.log("lastName")
+
+
+    }
